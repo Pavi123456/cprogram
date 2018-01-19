4b7a84ebedf9c384210a5fd44fc1c2594baef6b5
RJANANIESWARI <35128124+RJANANIESWARI@users.noreply.github.com>
2018-01-19 15:35:16 +0530

    Create palindrome.c

---
+++ palindrome.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+int i;
+char a[10],b[10];
+printf("enter the string");
+scanf("%s",a);
+strcpy(b,c);
+strrev(a);
+i=strcmp(a,b);
+if(i==0)
+{
+printf("palindrome");
+}
+else
+{
+printf("not palindrome");
+}
+}
