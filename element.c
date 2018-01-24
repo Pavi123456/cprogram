ba4d47f423c90ce82d2aeaf27a61fd42743a7987
RJANANIESWARI <35128124+RJANANIESWARI@users.noreply.github.com>
2018-01-24 15:47:41 +0530

    Create element.c

---
+++ element.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+int a[10],i,v;
+printf("Enter the size of array");
+scanf("%d",&a[i]);
+printf("Enter the elements");
+scanf("%d",&v);
+for(i=0;i<v;i++);
+{
+printf("%d%d",a[i],i);
+}
+}
