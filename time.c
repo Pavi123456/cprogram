dec37528e690c96b44244b7fb2353848074b628d
RJANANIESWARI <35128124+RJANANIESWARI@users.noreply.github.com>
2018-01-24 15:51:41 +0530

    Create time.c

---
+++ time.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+int min,hour,m1;
+printf("enter the time in min");
+scanf("%d",&minutes);
+hour=min/60;
+m1=min%60;
+printf("the hour and min are %d %d",hour,m1);
+}
