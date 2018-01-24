87b8c2554e54f02353202dbe1b9cf9f5b7984191
RJANANIESWARI <35128124+RJANANIESWARI@users.noreply.github.com>
2018-01-24 16:59:07 +0530

    Create numeric.c

---
+++ set4.c/numeric.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+char s[10];
+int i,count=0;
+printf("enter the string\n");
+scanf("%s",&s);
+for(i=0;s[i];i++)
+{
+if(s[i]=='0'||s[i]=='1'||s[i]=='2'||s[i]=='3'||s[i]=='4'||s[i]=='5'||s[i]=='6'||s[i]=='7'||s[i]=='8'||s[i]=='9')
+c++;
+{
+printf("The number of numeric characters :\n",count);
+}
+}
+}
