9bf43a1e90df147715edd488d34f0508bc34fac9
RJANANIESWARI <35128124+RJANANIESWARI@users.noreply.github.com>
2018-01-28 14:36:47 +0530

    Create 4.c

---
+++ set5/4.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+    int v,j;
+    printf("enter the no");
+    scanf("%d",&v);
+    for(j=1;j<=10;j++)
+    {
+        if(v<=10)
+        {
+        printf("yes\n");
+    }
+    else
+    {
+        printf("no\n");
+    }
+    }
+}
