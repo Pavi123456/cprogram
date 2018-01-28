bf54d437a498620836ea0b02dfb275aa307db53c
RJANANIESWARI <35128124+RJANANIESWARI@users.noreply.github.com>
2018-01-28 15:20:54 +0530

    Create 2.c

---
+++ pro/2.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+void main()
+{
+    int a[100],i,j,v,t,k;
+    scanf("%d",&v);
+    scanf("%d",&k);
+    for(i=0;i<v;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<v;i++)
+    {
+        for(j=i+1;j<v;j++)
+        if(a[i]>a[j])
+        {
+            t=a[i];
+            a[i]=a[j];
+            a[j]=t;
+        }
+    }
+    for(i=0;i<k-1;i++)
+   printf("%d ",a[i]);
+}
