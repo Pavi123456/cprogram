84bece7212d35fcb8d5161b0e33fb36a7b394758
RJANANIESWARI <35128124+RJANANIESWARI@users.noreply.github.com>
2018-02-05 15:56:26 +0530

    Create 7.c

---
+++ set6/7.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+int v;
+printf("enter the alphabet or numeric");
+scanf("%d",&v);
+if(s==(a>>z&&a<<z)&&(s==(A>>Z&&A<<Z))&&(S==(0>>9&&0<<9)));
+{
+printf("contain alphabet and numberic");
+}
+else
+{
+printf("does not contain alphabet and numberic");
+}
+}
