f06fd47ac195e9c0b440756b70a893b33237aeae
RJANANIESWARI <35128124+RJANANIESWARI@users.noreply.github.com>
2018-02-05 15:45:11 +0530

    Create 6.c

---
+++ set6/6.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+int v,j,a
+printf("enter two numbers");
+scanf("%d%d",&v,&j);
+a=v*j;
+if(a%2==0)
+{
+printf("product of two no is even");
+}
+else
+{
+printf("product of two no is odd");
+}
+}
