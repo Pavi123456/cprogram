317b5f8620e125bed836e24b7c4fa5795d0624b9
RJANANIESWARI <35128124+RJANANIESWARI@users.noreply.github.com>
2018-02-07 16:16:46 +0530

    Create set7.c

---
+++ set7.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+char a[10],i,n;
+printf("enter the values ");
+scanf("%s",&a);
+if(a[i]=='0'||a[i]=='1')
+{
+printf("binary representation");
+}
+else
+{
+printf("not a binary representation");
+}
+}
