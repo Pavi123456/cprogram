5bb03269d9868ab672ae7099b47416cee73b1178
RJANANIESWARI <35128124+RJANANIESWARI@users.noreply.github.com>
2018-02-19 15:55:50 +0530

    Create 9.c

---
+++ set8/9.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+void main()
+{
+int v,j,c,i;
+printf("enter the numbers");
+scanf("%d%d",&v,&j);
+c=v*j;
+for(i=0;i<c;i++)
+{
+if(c/i==i)
+{
+flag==1;
+}
+}
+if(flag==1)
+{
+printf("perfect square");
+}
+else
+{
+printf("not perfect square");
+}
+}
