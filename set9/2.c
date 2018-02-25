66f236af2b7abcd38c9f77411f187e192439a28a
RJANANIESWARI <35128124+RJANANIESWARI@users.noreply.github.com>
2018-02-25 16:39:53 +0530

    Create 2.c

---
+++ set9/2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void) {
+      float a,b,v;
+      printf("enter the length and breath\n");
+      scanf("%f%f",&a,&b);
+      v=a*b;
+      printf("area is %0.5f",v);
+	return 0;
+}
