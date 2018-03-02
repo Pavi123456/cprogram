b70fb01a08a2ebc5c1f4ac589a08cd6aff3f3d66
RJANANIESWARI <35128124+RJANANIESWARI@users.noreply.github.com>
2018-03-02 16:18:02 +0530

    Create 4.c

---
+++ set10/4.c
@@ -0,0 +1,16 @@
+
+#include<stdio.h>
+int main(void)
+{
+    int n,i,v,a[10],k;
+    printf("enter the limit");
+    scanf("%d",&n);
+    printf("enter the position");
+    scanf("%d",&v);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    printf("%d",a[k-1]);
+    return 0;
+}
