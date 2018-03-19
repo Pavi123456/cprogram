e53aba6abd030004ae0d523ac9de3917cfb804a4
RJANANIESWARI <35128124+RJANANIESWARI@users.noreply.github.com>
2018-03-19 16:54:47 +0530

    Create 2.c

---
+++ set12/2.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main(void)
+{
+	int a[10],b[10],i,j,c,d,s=0;
+	for(i=0;i<c;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	for(j=0;j<d;j++)
+	{
+		scanf("%d",&b[j]);
+	}
+	for(i=0;i<c;i++)
+	{
+		for(j=0;j<d;j++)
+		{
+			if(a[i]==b[j])
+			{
+				s++;
+			}
+
+		}
+	}
+	if(s==0)
+	{
+		printf("no");
+	}
+	if(s>0)
+	{
+		printf("yes");
+	}
+	return 0;
+}
